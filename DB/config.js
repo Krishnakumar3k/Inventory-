69ab18fd1c49f3b4f8c90ae47c42df0f49149d0c
Krishna Kumar Kushwaha <111342671+Krishnakumar3k@users.noreply.github.com>
2024-07-30 17:48:03 +0530

    Update config.js

--- DB/config.js
+++ DB/config.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://krishnakumar3kgt:kkk%40123@cluster0.ybcf3o4.mongodb.net/inventory").then(()=>console.log('Going Good Job!'))
+mongoose.connect("git ignore").then(()=>console.log('Going Good Job!'))
